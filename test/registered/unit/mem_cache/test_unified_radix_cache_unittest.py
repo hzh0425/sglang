14e7bd11241fe02d3e99f4919a9060454c685253
hzh0425 <hzh0425@apache.org>
2026-04-22 14:56:40 +0800

    test: relax host-hit assertion for evicted match

--- test/registered/unit/mem_cache/test_unified_radix_cache_unittest.py
+++ test/registered/unit/mem_cache/test_unified_radix_cache_unittest.py
@@ -1371,8 +1371,9 @@ class UnifiedRadixCacheSuite:
             DecLockRefParams(swa_uuid_for_lock=getattr(lr, "swa_uuid_for_lock", None)),
         )
         base_match = tree.match_prefix(MatchPrefixParams(key=RadixKey(base)))
+        self.assertTrue(base_node.evicted)
         self.assertIs(base_match.last_host_node, base_node)
-        self.assertGreaterEqual(base_match.host_hit_length, len(base))
+        self.assertIs(base_match.last_device_node, tree.root_node)
         tree.sanity_check()
 
     def test_hicache_d_leaf_h_leaf_mutual_exclusion(self):
